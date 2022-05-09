1d5b7802f8710888a5a8fe4d4dbda087a19a55b1
ritaly <malgorzata.lyczywek@gmail.com>
2022-05-09 20:02:51 +0200

    modify

--- 01_intro/learning.py
+++ 01_intro/learning.py
@@ -1,9 +1,8 @@
-from math import ceil
 
 learning_hours = 75
 my_time = 9
 
 full_course = learning_hours / my_time
-full_course = ceil(full_course)
+full_course = round(full_course, 1)
 
 print("Nauka zajmie mi: ", full_course, 'godz.')
