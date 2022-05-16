1622b43ebe0e4bd5be3a81e86ae7cb561e4fd67e
ritaly <malgorzata.lyczywek@gmail.com>
2022-05-16 20:04:13 +0200

    task 3

--- 03_flow_control/03.py
+++ 03_flow_control/03.py
@@ -1,1 +1,22 @@
-txt = "aBarKadabra123"
+txt = input('Podaj dowolny ciąg znaków -> ')
+
+counter_num = 0
+counter_up = 0
+counter_low = 0
+
+for l in txt:
+    if l.isdigit():
+        counter_num += 1
+        continue
+
+    if l.isupper():
+        counter_up += 1
+        continue
+
+    if l.islower():
+        counter_low += 1
+
+print(f'Text: {txt}')
+print("Cyfry", counter_num)
+print("Duże litery", counter_up)
+print("Małe litery", counter_low)
