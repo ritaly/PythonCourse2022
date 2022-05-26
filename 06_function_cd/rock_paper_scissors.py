d49db9d9f13f53210e711588f39249b2de65bac2
Rita <malgorzata.lyczycwek@gmail.com>
2022-05-26 23:25:59 +0200

    tic tac toe

--- 06_function_cd/rock_paper_scissors.py
+++ 06_function_cd/rock_paper_scissors.py
@@ -1,12 +1,15 @@
 import random
 
+# wygrany: (przegrany)
 WINNERS = {
-    'k': 'n',
-    'n': 'p',
-    'p': 'k'
+    'k': ('n', 'j'),
+    'n': ('p', 'j'),
+    'p': ('k', 's'),
+    'j': ('p', 's'),
+    's': ('n', 'k')
 }
 
-CORRECT_VALUES = ('k', 'p', 'n')
+CORRECT_VALUES = ('k', 'p', 'n', 'j', 's')
 
 
 def get_comp_choice():
@@ -15,7 +18,7 @@ def get_comp_choice():
 
 def get_user_choice():
     while True:
-        user_choice = input('Podaj wartość k - kamień, p - papier , n - nożyce: ')
+        user_choice = input('Podaj wartość k - kamień, p - papier , n - nożyce, s - Spock, j - jaszczurka: ')
         if user_choice in CORRECT_VALUES:
             break
 
@@ -25,7 +28,7 @@ def get_user_choice():
 def show_result(comp, user):
     if user == comp:
         print('Remis')
-    elif WINNERS[user] == comp:
+    elif comp in WINNERS[user]:
         print('Wygrywa użytkownika')
     else:
         print('Wygrywa komputer')
