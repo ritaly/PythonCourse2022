e10304e3ded08c35877e2b16727f7cc27a8328d7
Rita <malgorzata.lyczycwek@gmail.com>
2022-05-26 19:41:36 +0200

    pkn

---
+++ 06_function_cd/rock_paper_scissors.py
@@ -0,0 +1,49 @@
+import random
+
+WINNERS = {
+    'k': 'n',
+    'n': 'p',
+    'p': 'k'
+}
+
+CORRECT_VALUES = ('k', 'p', 'n')
+
+
+def get_comp_choice():
+    return random.choice(CORRECT_VALUES)
+
+
+def get_user_choice():
+    while True:
+        user_choice = input('Podaj wartość k - kamień, p - papier , n - nożyce: ')
+        if user_choice in CORRECT_VALUES:
+            break
+
+    return user_choice
+
+
+def show_result(comp, user):
+    if user == comp:
+        print('Remis')
+    elif WINNERS[user] == comp:
+        print('Wygrywa użytkownika')
+    else:
+        print('Wygrywa komputer')
+
+
+def main():
+    while True:
+        print('**** GRA K-P-N ****')
+
+        comp = get_comp_choice()
+        user = get_user_choice()
+        print(f'komputer {comp}, user {user}')
+        show_result(comp, user)
+
+        play_again = input("Czy zagrać ponownie? [T / N] -> ")
+        if play_again.upper() == 'N':
+            break
+
+    print('Dzięki za grę!')
+
+main()
